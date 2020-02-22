72a9a09b8d80c8b108c152c4cab51e51786cd37f
thiagocolen <tcolen@santander.com.br>
2020-02-22 14:46:20 -0300

    back-to-the-old-app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './store/configureStore'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-      </header>
-    </div>
-  );
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+
+import Home from './containers/Home'
+import './App.css'
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <MuiThemeProvider>
+          <BrowserRouter>
+            <React.Fragment>
+              <Route exact path="/" component={Home}/> 
+            </React.Fragment>
+          </BrowserRouter>
+        </MuiThemeProvider>
+      </Provider>
+    )
+  }
 }
 
-export default App;
+export default App
