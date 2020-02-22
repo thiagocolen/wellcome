4f40ab9a45ec5fd5f920ae86d50b36e58968007b
thiagocolen <tcolen@santander.com.br>
2020-02-22 15:09:28 -0300

    Updates

---
+++ precache-manifest.f4998a7855cb8e650533f26a633a512a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c89d871f0e05159ef813914c92fe20e0",
+    "url": "/wellcome/index.html"
+  },
+  {
+    "revision": "60f7cfac26f1901c9f35",
+    "url": "/wellcome/static/css/main.86b4b57e.chunk.css"
+  },
+  {
+    "revision": "bc6367b7cd3163a190de",
+    "url": "/wellcome/static/js/2.5633bb5f.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/wellcome/static/js/2.5633bb5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60f7cfac26f1901c9f35",
+    "url": "/wellcome/static/js/main.abea976b.chunk.js"
+  },
+  {
+    "revision": "8e7bac524dd317ca45b2",
+    "url": "/wellcome/static/js/runtime-main.48b2d760.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d79ea003416ab45bba42273889347a5b.js"
+  "/wellcome/precache-manifest.f4998a7855cb8e650533f26a633a512a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/wellcome/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
