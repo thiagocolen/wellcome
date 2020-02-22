64014efcbc1b2dfbcf4fcd322c0fae713d44932c
thiagocolen <tcolen@santander.com.br>
2020-02-22 13:48:58 -0300

    Updates

---
+++ precache-manifest.bdceb245abe07b777ab165ac7e17116b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93e060a30e36b2b7233042ff2beaed24",
+    "url": "/wellcome/index.html"
+  },
+  {
+    "revision": "24ebae83713f91c32a1b",
+    "url": "/wellcome/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "54cb3c065634ee0e5f8a",
+    "url": "/wellcome/static/js/2.cd7cac2e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/wellcome/static/js/2.cd7cac2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24ebae83713f91c32a1b",
+    "url": "/wellcome/static/js/main.fc76ae39.chunk.js"
+  },
+  {
+    "revision": "8e7bac524dd317ca45b2",
+    "url": "/wellcome/static/js/runtime-main.48b2d760.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wellcome/static/media/logo.5d5d9eef.svg"
+  }
+]);
