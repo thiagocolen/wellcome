de9588da2d441322b158d1acf8c50c2366ba113b
thiagocolen <tcolen@santander.com.br>
2020-02-29 11:00:18 -0300

    add-real-news-to-textBar-component

--- src/services/api.js
+++ src/services/api.js
@@ -1,24 +1,38 @@
-
 class Api {
 
-  
-  static getRandomPhoto = () => (
-    fetch('https://api.unsplash.com/photos/random/?client_id=7bb44829ebfb671742ab6f123c6581e0eca237754773b58d64940a82c82065f9')
-      .then((res) => {
-        return res.json()
-      })
-      .catch((err) => { 
-        console.log('Error happened during fetching!', err)
-        return err
-      })
-  )
+	static getNews = () => (
+		fetch(new Request(
+			'http://newsapi.org/v2/top-headlines?' +
+			'country=us&' +
+			'apiKey=d281384e581d44fda67ca1395c4d904a'
+		))
+		.then(function (res) {
+			return res.json()
+		})
+		.catch((err) => {
+			console.log('Error happened during fetching!', err)
+			return err
+		})		
+	)
+
+	
+	static getRandomPhoto = () => (
+		fetch('https://api.unsplash.com/photos/random/?client_id=7bb44829ebfb671742ab6f123c6581e0eca237754773b58d64940a82c82065f9')
+		.then((res) => {
+			return res.json()
+		})
+		.catch((err) => {
+			console.log('Error happened during fetching!', err)
+			return err
+		})
+	)
 
 
-  static getWeather = (latitude, longitude) => (
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=89b5e7279338d44e816ac45c030ac220`)
-      .then(res => res.json())
-      .catch(err => console.log('Error happened during fetching!', err))
-  )
+	static getWeather = (latitude, longitude) => (
+		fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=89b5e7279338d44e816ac45c030ac220`)
+		.then(res => res.json())
+		.catch(err => console.log('Error happened during fetching!', err))
+	)
 
 }
 
